7c333d8a48cdc2f578dccf175ffc5d5dcd309a16
Ricardo Dias <ricardodiasp2005@gmail.com>
2026-05-28 15:35:16 -0300

    Refactor GridView to include card categories and filtering

--- GridView.swift
+++ GridView.swift
@@ -7,18 +7,62 @@
 
 import SwiftUI
 
+
+
 struct GridView: View {
     let keyWord: String
 
     let rarityWord: [String] = ["Common", "Rare", "Epic", "Legendary", "Champion"]
     let typeWord: [String] = ["Troop", "Building", "Spell"]
-
+    
+    static let cardCategories: [String: [String]] = [
+        "goblin": [
+            "Goblins", "Spear Goblins", "Goblin Hut", "Goblin Barrel",
+            "Goblin Gang", "Goblin Giant", "Goblin Drill", "Goblin Demolisher",
+            "Goblin Machine", "Goblin Curse"
+        ],
+        
+        "aerial": [
+            "Minions", "Mega Minion", "Minion Horde", "Baby Dragon", "Inferno Dragon",
+            "Electro Dragon", "Skeleton Dragons", "Phoenix", "Lava Hound", "Bats"
+        ],
+        
+        "barbarian": [
+            "Barbarians", "Elite Barbarians", "Barbarian Hut", "Barbarian Barrel", "Battle Ram"
+        ],
+        
+        "skeleton": [
+            "Skeletons", "Skeleton Army", "Skeleton Barrel", "Skeleton Dragons",
+            "Bomber", "Wall Breakers", "Witch", "Night Witch", "Giant Skeleton",
+            "Skeleton King", "Graveyard", "Bomb Tower"
+        ],
+        
+        "tank": [
+            "Giant", "Golem", "P.E.K.K.A.", "Mega Knight", "Electro Giant",
+            "Royal Giant", "Lava Hound", "Giant Skeleton", "Elixir Golem"
+        ],
+        
+        "winCondition": [
+            "Giant", "Golem", "Electro Giant", "Royal Giant", "Lava Hound",
+            "Hog Rider", "Ram Rider", "Battle Ram", "Balloon", "Goblin Barrel",
+            "Goblin Drill", "Graveyard", "Miner", "Wall Breakers", "Three Musketeers",
+            "X-Bow", "Mortar", "Elixir Golem"
+        ],
+        
+        "evolved": [
+            "Skeletons", "Barbarians", "Mortar", "Royal Giant", "Knight",
+            "Recruits", "Archers", "Ice Spirit", "Valkyrie", "Bomber",
+            "Wall Breakers", "Zap", "Battle Ram", "Wizard", "Goblin Giant",
+            "Tesla", "Cage", "P.E.K.K.A", "Mega Knight", "Electro Dragon", "Musketeer"
+        ]
+    ]
+    
     var body: some View {
         if rarityWord.contains(keyWord) {
             GridCards(filterType: "rarity", typeInFilter: keyWord)
         } else if typeWord.contains(keyWord) {
             GridCards(filterType: "type", typeInFilter: keyWord)
-        } else {
+        }else{
             GridCards(filterType: "name", typeInFilter: keyWord)
         }
     }
@@ -49,12 +93,24 @@ struct GridCards: View {
                             count: columnsCount
                         ),
                         spacing: gridSpacing
-                    ) {
-                        ForEach(dataCards) { card in
-                            CardInGrid(actualCard: card)
+                    ){
+                        if typeInFilter == "goblinCards"{
+                            let filteredGoblins = dataCards.filter { card in
+                                goblinCards.contains(card.name)
+                            }
+                            
+                            
                         }
+                        ForEach(filteredGoblins, id: \.id) { card in
+                            CardInGrid(actualCard: card)
+                        }else{
+                            ForEach(dataCards) { card in
+                                CardInGrid(actualCard: card)
+                            }
+                            
+                        }
+                            .padding()
                     }
-                    .padding()
                 }
             }
         }
@@ -99,3 +155,4 @@ struct CardInGrid: View {
 #Preview {
     GridView(keyWord: "Common")
 }
+
